016cb1fcf42bd4f1ae129c4718c23f0aeed2a404
polinaantropova <st095276@spbu.student.ru>
2021-10-09 21:52:38 +0300

    решение задач со звёздочками

---
+++ bbm.bat
@@ -0,0 +1,70 @@
+@echo off
+
+setlocal enabledelayedexpansion
+
+echo.
+echo ===============
+echo.
+
+set string=
+
+for /L %%i in (1,1,%1) do ( 
+	for /L %%j in (1,1,%%i) do set string=!string!*
+	echo !string!
+	set string=
+)
+
+echo.
+echo ===============
+echo.
+
+set string=
+
+for /L %%i in (1,1,%1) do (
+	for /L %%j in (%1,-1,%%i) do set string=!string!*
+	echo !string!
+	set string=
+)
+
+echo.
+echo ===============
+echo.
+
+set string=
+
+for /L %%i in (1,1,%1) do (
+	set /a k=%1-%%i
+	for /L %%j in (!k!,-1,1) do set string=!string! 
+	for /L %%m in (1,1,%%i) do set string=!string!*
+	echo !string!
+	set string=
+)
+
+echo.
+echo ===============
+echo.
+
+set string=
+
+for /L %%i in (1,1,%1) do (
+	set /a k=%1-%%i
+	set /a r=2*%%i-1
+	for /L %%j in (1,1,!k!) do set string=!string! 
+	for /L %%m in (1,1,!r!) do set string=!string!*
+	echo !string!
+	set string=
+)
+
+set /a t=%1-1
+for /L %%i in (!t!,-1,1) do (
+	set /a k=%1-%%i
+	set /a r=2*%%i-1
+	for /L %%j in (%%i,1,!t!) do set string=!string! 
+	for /L %%m in (1,1,!r!) do set string=!string!*
+	echo !string!
+	set string=
+)
+
+echo.
+echo ===============
+echo.
